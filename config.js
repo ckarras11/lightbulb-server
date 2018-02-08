fb59d5566f36d02a2f9da9d25817e6588b74eb3a
Christopher Karras <ckarras11@gmail.com>
2018-02-08 13:07:18 -0500

    Added env vars for database url and port

--- config.js
+++ config.js
@@ -1,2 +1,9 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'postgresql://localhost/ideas';
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+global.DATABASE_URL ||
+'mongodb://localhost/lightbulb-server';
+
+exports.TEST_DATABASE_URL = (
+process.env.TEST_DATABASE_URL ||
+'mongodb://localhost/lightbulb-server');
+
 exports.PORT = process.env.PORT || 8080;
